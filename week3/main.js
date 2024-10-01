037108ba3ff90d9d5e71c079f80785ad12d29e9c
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-01 16:01:21 +0700

    week3

---
+++ week3/main.js
@@ -0,0 +1,17 @@
+//we write VueJS codes here
+
+//declare new Vue instance
+var app = new Vue({
+   el: "#app",   //el: element, #: id
+   data: {
+      message: "Hello Vue2 !",
+      overview: "VueJS - Progressive Frontend Framework"
+   }
+})
+
+var fpt = new Vue({
+   el: "#greenwich",
+   data: {
+      introduction: "Greenwich Vietnam is an alliance program between FPT Education and University of Greenwich"
+   }
+})
