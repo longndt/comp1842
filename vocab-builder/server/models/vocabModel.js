4c4383100ea52e0c25e5f4a26522f6e3ac9a73f5
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-11-05 16:23:29 +0700

    vocab

--- vocab-builder/server/models/vocabModel.js
+++ vocab-builder/server/models/vocabModel.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose')
 
 //declare schema (table design/structure)
 //we declare column (field) name + type along with input validation
-const vocabSchema = new mongoose.Schema({
-   english: {
-      type: String,
-      required: true
+const vocabSchema = new mongoose.Schema(
+   {
+      english: {
+         type: String,
+         required: true
+      },
+      german: {
+         type: String,
+         required: true
+      }
    },
-   german: {
-      type: String,
-      required: true
+   {
+      versionKey: false   //ignore "__v" when add new data
    }
-})
+)
 
 //declare model
 const vocabModel = mongoose.model('vocabs', vocabSchema)  //vocabs: table (collection) name
